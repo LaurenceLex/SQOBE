ae91e679d813bfdcc0733c1fcf3896918c570f4c
LaurenceLex <Info@posetone.com>
2025-08-19 20:24:47 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <header className="header">
-          <div className="logo">SQOBE</div>
+        <div className="logo">
+  <img src="/logo.png" alt="SQOBE Logo" height="36" />
+</div>
           <nav className="nav">
             <SearchBar />
           </nav>
